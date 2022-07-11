e77917a9b5b86e67389f7eaeb3a071374c2c8e23
yulchan <yulchan9478@gmail.com>
2022-07-11 10:24:11 +0900

    <feat> 새 기능 추가 (모달창)

---
+++ src/components/Roomenter.js
@@ -0,0 +1,387 @@
+import React from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import styled from "styled-components";
+import axios from "axios";
+
+const Login = ({ onClose }) => {
+  const outZone_ref = React.useRef(null);
+
+  const [todolist, setTodolist] = React.useState([]);
+  const [personinfo, setPersoninfo] = React.useState([]);
+
+  // todo 리스트 불러오기
+  // const TodoListAxios = () => {
+  //axios.defaults.withCredentials = true;
+  //   axios({
+  //     method: "get",
+  //     url: "/user/auth",
+  //     baseURL: "http://13.124.252.225",
+  // headers: {
+  //   authorization: localStorage.getItem("access_token"),
+  // },
+  //   })
+  //     .then(function (response) {
+  //       console.log(response);
+  //        setTodolist (response.data);
+  //     })
+  //     .catch(function (error) {
+  //       alert(error.response.data.message);
+  //       console.log(error);
+  //     });
+  // };
+
+  // 참여 멤버의  url과 닉네임불러오기
+  // const MemberlistAxios = () => {
+  //axios.defaults.withCredentials = true;
+  //   axios({
+  //     method: "get",
+  //     url: "/user/auth",
+  //     baseURL: "http://13.124.252.225",
+  // headers: {
+  //   authorization: localStorage.getItem("access_token"),
+  // },
+  //   })
+
+  //     .then(function (response) {
+  //
+  //
+  //      setPersoninfo(response.data)
+  //     })
+  //     .catch(function (error) {
+  //       alert(error.response.data.message);
+  //       console.log(error);
+  //     });
+  // };
+
+  return (
+    <Container>
+      <Background
+        ref={outZone_ref}
+        onClick={(e) => {
+          if (outZone_ref.current === e.target) {
+            onClose();
+          }
+        }}
+      >
+        <ModalBlock>
+          <Title> 자격증 공부</Title>
+          <Line />
+          <Label>
+            {/* 해당 방에 있는 TodoList 데이터 받아 map함수로 뿌리기 
+            {todolist.map((list,index) => {
+              return (
+                <div
+                key={index}>
+                <div>
+                {todolist}
+                </div>
+                </div>
+              )
+            })}
+            
+            */}
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <Chat1>Todo-List</Chat1>
+              <Todo>오늘은 4시간이상 하자</Todo>
+            </div>
+          </Label>
+          <Label>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              {/* 참여 인원 정보 닉네임과 url받아서 map 함수로 뿌리기
+              
+               {personinfo.map((list,index) => {
+              return (
+                 <div
+                    key={index}>
+                 <img
+                      src={list.iconUrl}
+                      style={{
+                        width: "50px",
+                        height: "50px",
+                        borderRadius: "10px",
+                      }}
+                    />
+                     <div style={{ textAlign: "left", margin: "0 10px" }}>
+                    <div>
+                    {list.nickname}
+                    </div>
+                    <div>
+                </div>
+              )
+            })}
+              
+              */}
+              <Chat2>참여인원</Chat2>
+
+              <div>
+                <People>
+                  <img
+                    style={{
+                      width: "25px",
+                      height: "25px",
+                      borderRadius: "50px",
+                      position: "relative",
+                      marginRight: "10px",
+                    }}
+                    src="https://opgg-com-image.akamaized.net/attach/images/20220220075306.1538486.jpg"
+                  />
+                  하율찬
+                </People>
+                <People>
+                  {" "}
+                  <img
+                    style={{
+                      width: "25px",
+                      height: "25px",
+                      borderRadius: "50px",
+                      position: "relative",
+                      marginRight: "10px",
+                    }}
+                    src="https://opgg-com-image.akamaized.net/attach/images/20220220075306.1538486.jpg"
+                  />
+                  기영석
+                </People>
+                <People>
+                  {" "}
+                  <img
+                    style={{
+                      width: "25px",
+                      height: "25px",
+                      borderRadius: "50px",
+                      position: "relative",
+                      marginRight: "10px",
+                    }}
+                    src="https://opgg-com-image.akamaized.net/attach/images/20220220075306.1538486.jpg"
+                  />
+                  조성인
+                </People>
+                <People>
+                  {" "}
+                  <img
+                    style={{
+                      width: "25px",
+                      height: "25px",
+                      borderRadius: "50px",
+                      position: "relative",
+                      marginRight: "10px",
+                    }}
+                    src="https://opgg-com-image.akamaized.net/attach/images/20220220075306.1538486.jpg"
+                  />
+                  조원희
+                </People>
+              </div>
+            </div>
+          </Label>
+          <EnterBtn>
+            <Btn1
+              onClick={() => {
+                onClose();
+              }}
+            >
+              취소
+            </Btn1>
+            <Btn2
+              onClick={() => {
+                onClose();
+                //방입장하는 navigate(방상세)
+              }}
+            >
+              입장하기
+            </Btn2>
+          </EnterBtn>
+
+          <LinkContainer></LinkContainer>
+        </ModalBlock>
+      </Background>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  z-index: 100;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const Background = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(255, 255, 255, 0.15);
+  backdrop-filter: blur(5px);
+`;
+
+const ModalBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  text-align: center;
+  position: absolute;
+  top: 6.5rem;
+  width: 509px;
+  height: 508px;
+  padding: 1.5rem;
+  background-color: white;
+  color: black;
+
+  box-shadow: 1px 1px 1px 1px gray;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  min-height: 35rem;
+  animation: modal-show 0.5s;
+  @keyframes modal-show {
+    from {
+      opacity: 0;
+      margin-top: -50px;
+    }
+    to {
+      opacity: 1;
+      margin-top: 0;
+    }
+`;
+const Title = styled.div`
+  font-size: 25px;
+  font-weight: bold;
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  text-align: center;
+  margin-top: 24px;
+`;
+const Line = styled.hr`
+  width: 410px;
+  height: 2px;
+  background-color: black;
+`;
+
+const Label = styled.label`
+  margin-top: 10px;
+  font-weight: bold;
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  text-align: center;
+`;
+
+const Chat1 = styled.div`
+  margin-right: 25px;
+  width: 84px;
+  height: 19px;
+  font-size: 18px;
+`;
+
+const Todo = styled.div`
+  background-color: gray;
+  width: 309px;
+  height: 36px;
+  font-size: 20px;
+`;
+
+const People = styled.div`
+  padding-top: 5px;
+  padding-left: 10px;
+  display: flex;
+  align-content: center;
+  text-align: center;
+  background-color: gray;
+  width: 300px;
+  height: 36px;
+  font-size: 20px;
+`;
+
+const Chat2 = styled.div`
+  display: flex;
+  width: 80px;
+  height: 22px;
+  margin-right: 27px;
+  font-weight: bold;
+`;
+const EnterBtn = styled.div`
+  margin-top: 20px;
+`;
+
+const LinkContainer = styled.div`
+  font-size: 13px;
+  color: #616061;
+  margin: auto;
+  align-content: center;
+`;
+
+const Btn1 = styled.button`
+border: 1px solid black;
+  margin-right:15px;
+  display: inline-block
+  margin-right: 10px;
+  margin-bottom: 12px;
+  width: 197px;
+  height: 50px;
+  color: black;
+  background-color: white;
+  
+  font-size: 18px;
+  font-weight: 900;
+  min-width: 96px;
+  padding: 0 16px 3px;
+  transition: all 80ms linear;
+  user-select: none;
+  outline: none;
+  cursor: pointer;
+  border-radius: 4px;
+  box-shadow: 0 1px 4px rgba(0, 0, 0, 0.3);
+
+  &:hover {
+    background-color: rgba(74, 21, 75, 0.9);
+    border: none;
+  }
+
+  &:focus {
+    --saf-0: rgba(var(--sk_highlight, 18, 100, 163), 1);
+    box-shadow: 0 0 0 1px var(--saf-0), 0 0 0 5px rgba(29, 155, 209, 0.3);
+  }
+`;
+
+const Btn2 = styled.button`
+display: inline-block
+  margin-bottom: 12px;
+  width: 197px;
+  height: 50px;
+  color: #fff;
+  background-color: black;
+  border: none;
+  font-size: 18px;
+  font-weight: 900;
+  min-width: 96px;
+  padding: 0 16px 3px;
+  transition: all 80ms linear;
+  user-select: none;
+  outline: none;
+  cursor: pointer;
+  border-radius: 4px;
+  box-shadow: 0 1px 4px rgba(0, 0, 0, 0.3);
+
+  &:hover {
+    background-color: rgba(74, 21, 75, 0.9);
+    border: none;
+  }
+
+  &:focus {
+    --saf-0: rgba(var(--sk_highlight, 18, 100, 163), 1);
+    box-shadow: 0 0 0 1px var(--saf-0), 0 0 0 5px rgba(29, 155, 209, 0.3);
+  }
+`;
+
+export default Login;
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,229 +1,338 @@
-import React from 'react';
-import { useNavigate,Link } from 'react-router-dom';
-import styled from 'styled-components'
-import axios from 'axios'
-import { storage } from '../shared/firebase';
-import { ref, uploadBytes, getDownloadURL } from 'firebase/storage'
-import Portal from './Portal'
+import React from "react";
+import styled from "styled-components";
+import axios from "axios";
+import { storage } from "../shared/firebase";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
+const SignUp = ({ onClose, LoginOpen }) => {
+  const outZone_ref = React.useRef(null);
+  const profile_ref = React.useRef(null); //유저 이미지 URL
+  const [profile, setprofile] = React.useState(
+    //유저 이미지 URL
+    "https://opgg-com-image.akamaized.net/attach/images/20220220075306.1538486.jpg"
+  );
+  const [email, setemail] = React.useState(""); //email 인풋
+  const [password, setPwd] = React.useState(""); //비밀번호 인풋
+  const [passwordCheck, setpasswordCheck] = React.useState(""); //비밀번호 확인 인풋
+  const [nickname, setNickName] = React.useState(""); //닉네임인풋
 
-const SignUp = ( {onClose} ) => {
-
-   
-    const navigate = useNavigate();
-
-    const profile_ref = React.useRef(null);
-    const id_ref = React.useRef(null);
-    const pw_ref = React.useRef(null);
-    const pw_check_ref = React.useRef(null);
-    const nickname_ref = React.useRef(null);
-    const outZone_ref = React.useRef();
-
-    const [profile,setprofile] = React.useState("https://opgg-com-image.akamaized.net/attach/images/20220220075306.1538486.jpg");
-    const [username, setUsername] = React.useState("");
-    const [password, setPwd] = React.useState("");
-    const [pwdCheck, setPwdCheck] = React.useState("");
-    const [nickname, setNickName] = React.useState("");
-
-   
-
-    const UpImageUrl = async (e) => {
-
-        
-        const upload_file = await uploadBytes(
-            ref(storage, `images/${e.target.files[0].name}`),
-            e.target.files[0]
-        );
-
-        
-        const file_url = await getDownloadURL(upload_file.ref)
-        profile_ref.current = { url: file_url };
-        setprofile(profile_ref.current.url)
-        
-    };
-
-
-    const signupdata = () => {
-       
-          
-        axios({
-
-             method: 'POST',            
-             url: "/signup", 
-             data:{
-                username: username,
-                password: password,
-                passwordCheck: pwdCheck ,
-                nickname: nickname,
-                iconUrl: profile,
-             },
-             baseURL:"url"
-         }).then((response) => {
-                console.log(response)
-            
-            alert("회원가입을 축하드립니다!!")
-            navigate('/login');
-
-        }).catch((error) => {
-            console.log(error)
-            alert(error.response.data.message)
-        })
-
-    }
-    
-    const handlerId = (e) => {
-        setUsername(e.target.value);
-    }
-
-    const handlerPw = (e) => {
-        setPwd(e.target.value);
-    }
-
-
-    const handlerNickName = (e) => {
-        setNickName(e.target.value);
-    }
-  
-    const handlerPwcheck = (e) => {
-        setPwdCheck(e.target.value);
-    }
-    
- 
-  
-    return (
-        <Portal>
-            <Container >
-        <Background ref ={outZone_ref} onClick={(e)=>{
-            if (outZone_ref.current===e.target){
-                onClose()
-            }
-        }}>
-        <ModalBlock >
-            <LinkContainer>
-                이미 회원이신가요? <br />
-                <Link to="/login">로그인하러가기</Link>
-            </LinkContainer>
-            <button onClick={onClose}> X</button>
-            
-            <Form>
-                <Label >
-                    <span>이메일 주소</span>
-                    <div>
-                        <Input type="email"  placeholder="abc@example.com" ref={id_ref} onChange={handlerId} />
-                    </div>
-                </Label>
-                <Label>
-                    <span>닉네임</span>
-                    <div>
-                        <Input type="text" ref={nickname_ref}  onChange={handlerNickName} />
-                    </div>
-                    <Label>
-                        <span>프로필사진</span>
-                        <span>
-                        <Inlabel htmlFor="files">사진 업로드</Inlabel> 
-                        <img style={{width:'256px', height:'156px'}} src={profile} />
-                        <Input2 type="file" id="files" ref={profile_ref} onChange={UpImageUrl} /> <br />
-                        </span>
-                      
-                    </Label>
-                </Label>
-                <Label >
-                    <span>비밀번호</span>
-                    <div>
-                        <Input type="password" ref={pw_ref} onChange={handlerPw} />
-                    </div>
-                </Label>
-                <Label >
-                    <span>비밀번호 확인</span>
-                    <div>
-                        <Input
-                            type="password"
-                            onChange={handlerPwcheck}
-                            ref={pw_check_ref}
-                        />
-                    </div>
-
-                </Label>
-                <Button onClick={signupdata} >회원가입</Button>
-            </Form>
-          </ModalBlock >
-         </Background> 
-         </Container>
-         </Portal>
+  //firebase 사용해서 url 추출
+  const UpImageUrl = async (e) => {
+    const upload_file = await uploadBytes(
+      ref(storage, `images/${e.target.files[0].name}`),
+      e.target.files[0]
     );
-}
+
+    const file_url = await getDownloadURL(upload_file.ref);
+    profile_ref.current = { url: file_url };
+    setprofile(profile_ref.current.url);
+  };
+
+  // 회원가입 통신
+  const signupdata = () => {
+    axios({
+      method: "POST",
+      url: "/api/auth/signup",
+      data: {
+        email: email,
+        password: password,
+        passwordCheck: passwordCheck,
+        nickname: nickname,
+        iconUrl: profile,
+      },
+      baseURL: "http://3.35.26.55",
+    })
+      .then((response) => {
+        console.log(response);
+
+        alert("회원가입을 축하드립니다!!");
+        onClose();
+        LoginOpen();
+      })
+      .catch((error) => {
+        console.log(error);
+        alert(error.response.data.message);
+      });
+  };
+
+  const handlerId = (e) => {
+    setemail(e.target.value);
+  };
+
+  const handlerPw = (e) => {
+    setPwd(e.target.value);
+  };
+
+  const handlerNickName = (e) => {
+    setNickName(e.target.value);
+  };
+
+  const handlerPwcheck = (e) => {
+    setpasswordCheck(e.target.value);
+  };
+
+  return (
+    <Container>
+      <Background
+        ref={outZone_ref}
+        onClick={(e) => {
+          if (outZone_ref.current === e.target) {
+            onClose();
+          }
+        }}
+      >
+        <ModalBlock>
+          <Title>회원가입</Title>
+          <Line />
+          <Label>
+            <span>
+              <img
+                alt=""
+                style={{
+                  cursor: "pointer",
+                  width: "80px",
+                  height: "80px",
+                  borderRadius: "50px",
+                  position: "relative",
+                }}
+                src={profile}
+              />
+              <img
+                alt=""
+                style={{
+                  cursor: "pointer",
+                  width: "28px",
+                  borderRadius: "50px",
+                  position: "absolute",
+                  marginLeft: "-30px",
+                  marginTop: "53px",
+                }}
+                src="https://www.shareicon.net/data/2017/05/09/885771_camera_512x512.png"
+              />
+              <Input
+                style={{ display: "none" }}
+                type="file"
+                id="files"
+                onChange={UpImageUrl}
+              />
+              <br />
+            </span>
+          </Label>
+          <Label>
+            <div>
+              <Chat1>이메일</Chat1>
+
+              <Input
+                type="email"
+                placeholder="abc@example.com"
+                onChange={handlerId}
+              />
+            </div>
+          </Label>
+
+          <Label>
+            <div>
+              <Chat2>비밀번호</Chat2>
+              <Input
+                style={{ fontSize: "15px" }}
+                type="password"
+                onChange={handlerPw}
+                placeholder="숫자, 영문, 특수문자 조합 최소 8자"
+              />
+            </div>
+          </Label>
+          <Label>
+            <div>
+              <Chat3></Chat3>
+              <Input
+                type="password"
+                style={{ fontSize: "15px", marginRight: "8px" }}
+                placeholder="비밀번호 확인"
+                onChange={handlerPwcheck}
+              />
+            </div>
+          </Label>
+          <Label>
+            <div>
+              <Chat1>닉네임</Chat1>
+
+              <Input type="text" onChange={handlerNickName} />
+            </div>
+          </Label>
+          <LoginBtn>
+            <Button1 onClick={signupdata}>회원가입</Button1> <br />
+            <Button2
+              onClick={() => {
+                onClose();
+              }}
+            >
+              취소
+            </Button2>
+          </LoginBtn>
+        </ModalBlock>
+      </Background>
+    </Container>
+  );
+};
 const Container = styled.div`
-    position: fixed;
-    width: 100%;
-    height: 100%;
-    z-index: 100;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  z-index: 100;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const Background = styled.div`
-    position: fixed;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(255,255,255,0.15);
-    backdrop-filter: blur(5px);
-    
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(255, 255, 255, 0.15);
+  backdrop-filter: blur(5px);
 `;
 
 const ModalBlock = styled.div`
+font-family: Pretendard;
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  text-align: center;
+  position: absolute;
+  background-color: white;
+  color: black;
+  width: 458px;
+  height: 672px;
+  box-shadow: 1px 1px 1px 1px gray;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 
-    position: absolute;
-    top: 6.5rem;
-    border-radius: 10px;
-    padding: 1.5rem;
-    background-color: white;
-    color: black;
-    width: 700px;
-    box-shadow: 1px 1px 1px 1px gray;
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
+  animation: modal-show 0.5s;
+  @keyframes modal-show {
+    from {
+      opacity: 0;
+      margin-top: -50px;
+    }
+    to {
+      opacity: 1;
+      margin-top: 0;
+    }
 `;
 
-const Inlabel =styled.label`
-    
-`
-
-const Input2=styled.input`
- 
-`
-
-
-const Header = styled.header`
-  
+const Title = styled.div`
+  font-size: 26px;
+  font-weight: bold;
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  text-align: center;
+  margin-top: 24px;
 `;
 
-const Form = styled.div`
-
+const Line = styled.hr`
+  background-color: black;
+  width: 360px;
+  height: 2px;
+  margin-top: 12px;
+  margin-bottom: 32px;
 `;
 
 const Label = styled.label`
- 
+  font-weight: bold;
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  text-align: center;
+`;
+const Chat1 = styled.span`
+  font-size: 20px;
+  margin-right: 25px;
 `;
 
+const Chat2 = styled.span`
+  font-size: 18px;
+  margin-right: 14px;
+  vertical-align: middle;
+`;
+const Chat3 = styled.span`
+  margin-right: 85px;
+`;
 const Input = styled.input`
- 
+  margin-top: 16px;
+  --saf-0: rgba(var(--sk_foreground_high_solid, 134, 134, 134), 1);
+  border: 1px solid var(--saf-0);
+  transition: border 80ms ease-out, box-shadow 80ms ease-out;
+  box-sizing: border-box;
+  width: 280px;
+  height: 36px;
+  color: rgba(var(--sk_primary_foreground, 29, 28, 29), 1);
+  background-color: rgba(var(--sk_primary_background, 255, 255, 255), 1);
+  padding: 12px;
+  border-radius: 4px;
+  font-size: 18px;
+  line-height: 1.33333333;
+`;
+const LoginBtn = styled.div``;
+
+const Button1 = styled.button`
+  margin-top: 42px;
+  color: #fff;
+  background-color: black;
+  border: none;
+  font-size: 18px;
+  font-weight: 900;
+  width: 360px;
+  height: 50px;
+  padding: 0 16px 3px;
+  transition: all 80ms linear;
+  user-select: none;
+  outline: none;
+  cursor: pointer;
+  border-radius: 4px;
+  box-shadow: 0 1px 4px rgba(0, 0, 0, 0.3);
+  &:hover {
+    background-color: rgba(74, 21, 75, 0.9);
+    border: none;
+  }
+  &:focus {
+    --saf-0: rgba(var(--sk_highlight, 18, 100, 163), 1);
+    box-shadow: 0 0 0 1px var(--saf-0), 0 0 0 5px rgba(29, 155, 209, 0.3);
+  }
 `;
 
-const Button = styled.button`
-
-`
-
-
-
-export const LinkContainer = styled.p`
-
+const Button2 = styled.button`
+  align-content: center;
+  text-align: center;
+  margin-top: 8px;
+  color: black;
+  background-color: white;
+  border: none;
+  font-size: 18px;
+  font-weight: 900;
+  width: 360px;
+  height: 50px;
+  border: 1px solid black;
+  padding: 0 16px 3px;
+  transition: all 80ms linear;
+  user-select: none;
+  outline: none;
+  cursor: pointer;
+  border-radius: 4px;
+  box-shadow: 0 1px 4px rgba(0, 0, 0, 0.3);
+  &:hover {
+    background-color: rgba(74, 21, 75, 0.9);
+    border: none;
+  }
+  &:focus {
+    --saf-0: rgba(var(--sk_highlight, 18, 100, 163), 1);
+    box-shadow: 0 0 0 1px var(--saf-0), 0 0 0 5px rgba(29, 155, 209, 0.3);
+  }
 `;
 
-export default SignUp
+export default SignUp;
