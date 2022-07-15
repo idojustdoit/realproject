8e4578d7f47744f7d70e2070cb5ed901072817d2
yulchan <yulchan9478@gmail.com>
2022-07-16 00:01:20 +0900

    {feat} 새기능 추가 소셜 로그인, 그래프 컴포넌트 추가

--- src/pages/Kakaologin.js
+++ src/pages/Kakaologin.js
@@ -8,16 +8,11 @@ function Kakaologin() {
     let params = new URL(document.location.toString()).searchParams;
     let code = params.get("code"); // 인가코드 받는 부분
     let grant_type = "authorization_code";
-    let client_id = "75f121198efee416687964ae4caad0d5";
+    let client_id = "b266efe96498090868c78833faf62705";
     console.log(code);
     axios
       .post(
-        `https://kauth.kakao.com/oauth/token?
-        grant_type=${grant_type}
-        &client_id=${client_id}
-        &redirect_uri=http://localhost:3000/Kakaologin
-        &code=${code}
-        `,
+        `https://kauth.kakao.com/oauth/token?grant_type=${grant_type}&client_id=${client_id}&redirect_uri=http://localhost:3000/Kakaologin&code=${code}`,
         {
           headers: {
             "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
@@ -26,9 +21,24 @@ function Kakaologin() {
       )
       .then((res) => {
         console.log(res);
-        sessionStorage.setItem("access_token", res.data.accessToken);
-        sessionStorage.setItem("refresh_token", res.data.refreshToken);
-        navigate("/");
+        const access_token = res.data.access_token;
+        const refresh_token = res.data.refresh_token;
+
+        axios.post("url", { access_token, refresh_token }).then((res) => {
+          const user_id = res.data.user_id;
+          const user_nickname = res.data.kakao_profile.user_nickname;
+          const user_url = res.data.kakao_profile.image;
+
+          axios
+            .post("url", { user_id, user_nickname, user_url })
+            .then((res) => {
+              console.log(res);
+              localStorage.setItem("accessToken", res.data.accessToken);
+              localStorage.setItem("refreshToken", res.data.refreshToken);
+              localStorage.setItem("userId", res.data.userId);
+              navigate("/");
+            });
+        });
       });
   }, []);
   return <div> Kakaologin</div>;
